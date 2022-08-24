2ca49030c5a0d8587cc45d332c7b362ca90b6407
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-24 08:17:51 -0600

    function in a function scope

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 
 function calcAge(birthYear) {
   const age = 2022 - birthYear;
-  console.log(firstName); //it will available because we define it in global scope
+  //console.log(firstName); //it will available because we define it in global scope
+  function printAge() {
+    const output = `You're ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
   return age;
 }
 
